7f879f48f88bb641082102e87ec88a0c7fd5a3c2
ChanGL <130426257+ChanGLCorpse@users.noreply.github.com>
2023-07-11 11:27:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////sqliteServer/sheep.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////root/sqliteServer/sheep.db'
 db = SQLAlchemy(app)
 
 class Data(db.Model):
